51ec6a5b26d48e00a5ac7761bdc40579a01fe3fe
Jerome Marbach <jeromemarbach@outlook.fr>
2025-05-20 12:55:12 +0200

    I am starting the exercise again

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import { useState } from "react";
+import EmployeeCard from "./components/EmployeeCard";
 import "./App.css";
 
-import EmployeeCard from "./components/EmployeeCard";
-
 const sampleEmployee = {
-	name:{
-		first: "Charlie",
-		last: "Thompson",
-	},
-	email: "charlie.thompson@example.com",
-	picture:{medium :"https://randomuser.me/api/portraits/med/men/40.jpg",
-
-	},
-}
-
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: { medium: "https://randomuser.me/api/portraits/med/men/40.jpg" },
+};
 
 function App() {
-	const [employee, setEmployee] = useState(sampleEmployee)
-	const getEmployee = async () => {
-		// Send the request
-		const response = await fetch("http://localhost:3310/api/employees")
-		const result = await response.json()
-		  console.log(result)
-			setEmployee (result.results[0]);
-		  };
-	return (
-		<div className="app">
-			<EmployeeCard employee={employee} />
-			<button type="button" onClick={getEmployee}>Next Employee</button>
-		</div>
-	);
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  return (
+    <div>
+      <EmployeeCard employee={sampleEmployee} />
+    </div>
+  );
 }
 
 export default App;
